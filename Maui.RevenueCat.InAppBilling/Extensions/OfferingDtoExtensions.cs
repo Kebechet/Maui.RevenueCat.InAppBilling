459dcd924db422e636cecab9adfeaac8c2169f07
Samuel Sidor <samosidor@gmail.com>
2023-05-13 17:48:39 +0200

    added public extension helpers

---
+++ Maui.RevenueCat.InAppBilling/Extensions/OfferingDtoExtensions.cs
@@ -0,0 +1,53 @@
+﻿using Maui.RevenueCat.InAppBilling.Models;
+
+namespace Maui.RevenueCat.InAppBilling.Extensions;
+
+public static class OfferingDtoExtensions
+{
+    private static readonly decimal _daysInWeek = 7m;
+    private static readonly decimal _daysInMonth = 30m;
+    private static readonly decimal _monthsInBiMonthly = 2m;
+    private static readonly decimal _quartalsInYear = 4m;
+    private static readonly decimal _monthsInHalfYear = 6m;
+    private static readonly decimal _monthsInYear = 12m;
+
+    public static decimal GetMonthlyPrice(this OfferingDto offeringDto)
+    {
+        if (offeringDto.Identifier == DefaultOfferingIdentifier.Weekly)
+        {
+            return offeringDto.Product.Price / _daysInWeek * _daysInMonth;
+        }
+
+        if (offeringDto.Identifier == DefaultOfferingIdentifier.Monthly)
+        {
+            return offeringDto.Product.Price;
+        }
+
+        if (offeringDto.Identifier == DefaultOfferingIdentifier.BiMonthly)
+        {
+            return offeringDto.Product.Price / _monthsInBiMonthly;
+        }
+
+        if (offeringDto.Identifier == DefaultOfferingIdentifier.Quarterly)
+        {
+            return offeringDto.Product.Price / _quartalsInYear;
+        }
+
+        if (offeringDto.Identifier == DefaultOfferingIdentifier.SemiAnnually)
+        {
+            return offeringDto.Product.Price / _monthsInHalfYear;
+        }
+
+        if (offeringDto.Identifier == DefaultOfferingIdentifier.Annually)
+        {
+            return offeringDto.Product.Price / _monthsInYear;
+        }
+
+        throw new NotImplementedException();
+    }
+
+    public static string GetMonthlyPriceWithCurrency(this OfferingDto offeringDto)
+    {
+        return offeringDto.Product.PriceWithCurrency.Replace($"{offeringDto.Product.Price:n2}", $"{offeringDto.GetMonthlyPrice():n2}");
+    }
+}
---
+++ Maui.RevenueCat.InAppBilling/Models/DefaultOfferingIdentifiers.cs
@@ -0,0 +1,13 @@
+﻿namespace Maui.RevenueCat.InAppBilling.Models;
+
+//https://github.com/RevenueCat/purchases-ios/blob/main/Sources/Purchasing/PackageType.swift#L66
+public static class DefaultOfferingIdentifier
+{
+    public static string Weekly => "$rc_weekly";
+    public static string Monthly => "$rc_monthly";
+    public static string BiMonthly => "$rc_two_month";
+    public static string Quarterly => "$rc_three_month";
+    public static string SemiAnnually => "$rc_six_month";
+    public static string Annually => "$rc_annual";
+    public static string Lifetime => "$rc_lifetime";
+}
