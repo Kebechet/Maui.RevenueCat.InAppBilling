b04f613cfd1072a657723c26a3d30154b5ffbe1c
Samuel Sidor <samosidor@gmail.com>
2023-05-31 16:56:51 +0200

    fixed GetMonthlyPriceWithCurrency

--- Maui.RevenueCat.InAppBilling/Extensions/OfferingDtoExtensions.cs
+++ Maui.RevenueCat.InAppBilling/Extensions/OfferingDtoExtensions.cs
@@ -1,8 +1,9 @@
 ﻿using Maui.RevenueCat.InAppBilling.Models;
+using System.Text.RegularExpressions;
 
 namespace Maui.RevenueCat.InAppBilling.Extensions;
 
-public static class OfferingDtoExtensions
+public static partial class OfferingDtoExtensions
 {
     private static readonly decimal _daysInWeek = 7m;
     private static readonly decimal _daysInMonth = 30m;
@@ -10,6 +11,13 @@ public static class OfferingDtoExtensions
     private static readonly decimal _quartalsInYear = 4m;
     private static readonly decimal _monthsInHalfYear = 6m;
     private static readonly decimal _monthsInYear = 12m;
+
+    [GeneratedRegex(@"(\d+)[\.\,]{1}")]
+    private static partial Regex BeforeSeparatorValueRegex();
+    [GeneratedRegex(@"[\.\,]{1}(\d+)")]
+    private static partial Regex AfterSeparatorValueRegex();
+    [GeneratedRegex(@"\d+([\.\,]{1})\d+")]
+    private static partial Regex SeparatorValueRegex();
 
     public static decimal GetMonthlyPrice(this OfferingDto offeringDto, bool ignoreExceptions = true)
     {
@@ -55,8 +63,13 @@ public static class OfferingDtoExtensions
     {
         try
         {
-            return offeringDto.Product.Pricing.PriceLocalized
-            .Replace($"{offeringDto.Product.Pricing.Price:n2}", $"{offeringDto.GetMonthlyPrice(ignoreExceptions):n2}");
+            //the reason for not using directly CultureInfo.CurrentCulture.NumberFormat.CurrencyDecimalSeparator
+            //is to persist formatting of the price as it is in the Payment vendor
+            var monthlyPrice = $"{offeringDto.GetMonthlyPrice(ignoreExceptions):n2}";
+            var monthlyPriceBeforeSeparator = BeforeSeparatorValueRegex().Match(monthlyPrice).Groups[1].Value;
+            var monthlyPriceAfterSeparator = AfterSeparatorValueRegex().Match(monthlyPrice).Groups[1].Value;
+
+            return SeparatorValueRegex().Replace(offeringDto.Product.Pricing.PriceLocalized, $"{monthlyPriceBeforeSeparator}${{1}}{monthlyPriceAfterSeparator}");
         }
         catch
         {
