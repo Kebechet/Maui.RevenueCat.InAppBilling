c9f298ac574d4df6f90d6f36974d41346ff788e8
Cameron Vetter <cameron@cameronvetter.com>
2023-09-13 09:10:41 -0500

    moved extension

--- Maui.RevenueCat.InAppBilling/Enums/IntroElegibilityStatus.cs
+++ Maui.RevenueCat.InAppBilling/Enums/IntroElegibilityStatus.cs
@@ -1,6 +1,3 @@
-#if IOS
-using Maui.RevenueCat.iOS;
-#endif
 namespace Maui.RevenueCat.InAppBilling.Enums;
 
 public enum IntroElegibilityStatus
@@ -10,24 +7,3 @@ public enum IntroElegibilityStatus
     NoIntroOfferExists,
     Unknown
 }
-
-#if IOS
-public static class Extensions
-{
-    public static IntroElegibilityStatus Convert(this RCIntroEligibilityStatus eligibility)
-    {
-        switch (eligibility)
-        {
-            case RCIntroEligibilityStatus.Ineligible:
-                return IntroElegibilityStatus.Ineligible;
-            case RCIntroEligibilityStatus.Eligible:
-                return IntroElegibilityStatus.Eligible;
-            case RCIntroEligibilityStatus.NoIntroOfferExists:
-                return IntroElegibilityStatus.NoIntroOfferExists;
-            case RCIntroEligibilityStatus.Unknown:
-            default:
-                return IntroElegibilityStatus.Unknown;
-        }
-    }
-}
-#endif
---
+++ Maui.RevenueCat.InAppBilling/Platforms/iOS/Extensions/IntroElegibilityStatusExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Maui.RevenueCat.InAppBilling.Enums;
+using Maui.RevenueCat.iOS;
+
+namespace Maui.RevenueCat.InAppBilling.Extensions;
+internal static class IntroElegibilityStatusExtensions
+{
+    public static IntroElegibilityStatus Convert(this RCIntroEligibilityStatus eligibility)
+    {
+        switch (eligibility)
+        {
+            case RCIntroEligibilityStatus.Ineligible:
+                return IntroElegibilityStatus.Ineligible;
+            case RCIntroEligibilityStatus.Eligible:
+                return IntroElegibilityStatus.Eligible;
+            case RCIntroEligibilityStatus.NoIntroOfferExists:
+                return IntroElegibilityStatus.NoIntroOfferExists;
+            case RCIntroEligibilityStatus.Unknown:
+            default:
+                return IntroElegibilityStatus.Unknown;
+        }
+    }
+}
