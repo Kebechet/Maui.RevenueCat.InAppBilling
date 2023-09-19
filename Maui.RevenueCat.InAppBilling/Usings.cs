5c72a96efa9ff90821f34ae9f135b67c5262dab1
Samuel Sidor <samosidor@gmail.com>
2023-09-19 19:41:53 +0200

    supressed IDE0005 using warning

--- Maui.RevenueCat.InAppBilling/Usings.cs
+++ Maui.RevenueCat.InAppBilling/Usings.cs
@@ -1,1 +1,3 @@
-﻿global using LogLevel = Maui.RevenueCat.InAppBilling.Enums.LogLevel;
+﻿#pragma warning disable IDE0005 // Using directive is unnecessary.
+global using LogLevel = Maui.RevenueCat.InAppBilling.Enums.LogLevel;
+#pragma warning restore IDE0005 // Using directive is unnecessary.
