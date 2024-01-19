e119e7de38b2d4afc2ace3591e4cb2307c060c37
Kebechet <samosidor@gmail.com>
2024-01-19 11:56:38 +0100

    added simple showcase functionality

--- demo/DemoApp/DemoApp/MauiProgram.cs
+++ demo/DemoApp/DemoApp/MauiProgram.cs
@@ -14,6 +14,7 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
+        builder.Services.AddTransient<MainPage>();
 
 #if DEBUG
 		builder.Logging.AddDebug();
