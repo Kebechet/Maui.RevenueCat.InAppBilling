de1f1fa1bcc414d6fc9964b7f79514ef83c11100
Kebechet <samosidor@gmail.com>
2024-01-19 12:20:27 +0100

    test commit

---
+++ tests/Maui.RevenueCat.InAppBilling.Unit.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Maui.RevenueCat.InAppBilling.Unit.Tests;
+
+//https://learn.microsoft.com/en-us/dotnet/architecture/maui/unit-testing
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
