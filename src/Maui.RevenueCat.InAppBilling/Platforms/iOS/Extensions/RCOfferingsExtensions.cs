062f8dbec936eeabe8e52e89ad8f8dbc81f454a1
Will <willbeal@outlook.com>
2024-02-12 18:39:46 +0100

    Added Metadata to Offerings
    Added a Dictionary of Metadata to OfferingDto. Implemented platform methods to retrieve the metadata and convert the native classes to dotnet classes.

--- src/Maui.RevenueCat.InAppBilling/Platforms/iOS/Extensions/RCOfferingsExtensions.cs
+++ src/Maui.RevenueCat.InAppBilling/Platforms/iOS/Extensions/RCOfferingsExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Maui.RevenueCat.InAppBilling.Models;
+﻿using Foundation;
+using Maui.RevenueCat.InAppBilling.Models;
 using Maui.RevenueCat.iOS;
 
 namespace Maui.RevenueCat.InAppBilling.Platforms.iOS.Extensions;
@@ -15,12 +16,68 @@ internal static class RCOfferingsExtensions
             {
                 Identifier = offer.Identifier,
                 AvailablePackages = offer.AvailablePackages.ToPackageDtoList(),
-                IsCurrent = offer.Identifier == offerings.Current?.Identifier
+                IsCurrent = offer.Identifier == offerings.Current?.Identifier,
+                Metadata = offer.Metadata.ToDictionary<KeyValuePair<NSString, NSObject>, string, object?>(item => item.Key,
+                    item => item.Value.ToDotNetObject())
             };
-
             offeringDtos.Add(offeringDto);
         }
 
         return offeringDtos;
+    }
+
+    private static object? ToDotNetObject(this NSObject nsObj) =>
+        nsObj switch
+        {
+            NSString stringValue => stringValue.ToString(), // string
+            NSDecimalNumber d => (decimal)d.NSDecimalValue, // decimal
+            NSNumber numberValue => numberValue.ObjCType switch
+            {
+                "c" when numberValue.Class.Name == "__NSCFBoolean" => numberValue.BoolValue, // ObjC bool
+                "c" => numberValue.SByteValue, // signed char
+                "i" => numberValue.Int32Value, // signed int
+                "s" => numberValue.Int16Value, // signed short
+                "l" => numberValue.Int32Value, // signed long (32 bit)
+                "q" => numberValue.Int64Value, // signed long long (64 bit)
+                "C" => numberValue.ByteValue, // unsigned char
+                "I" => numberValue.UInt32Value, // unsigned int
+                "S" => numberValue.UInt16Value, // unsigned short
+                "L" => numberValue.UInt32Value, // unsigned long (32 bit)
+                "Q" => numberValue.UInt64Value, // unsigned long long (64 bit)
+                "f" => numberValue.FloatValue, // float
+                "d" => numberValue.DoubleValue, // double
+                "B" => numberValue.BoolValue, // C++ bool or C99 _Bool
+                _ => numberValue.ToString() // fallback
+            },
+            NSArray arrayValue => arrayValue.ToDotNetList(),
+            NSDictionary dictValue => dictValue.ToDotNetDictionary(),
+            NSNull => null,
+            _ => nsObj.ToString() // fallback
+        };
+
+    private static Dictionary<object, object?> ToDotNetDictionary(this NSDictionary nsDictionary)
+    {
+        var dotNetDictionary = new Dictionary<object, object?>();
+
+        foreach (var key in nsDictionary.Keys)
+        {
+            var value = nsDictionary[key];
+            dotNetDictionary.Add(key.ToDotNetObject() ?? throw new NullReferenceException("Key cannot be null."), value.ToDotNetObject());
+        }
+
+        return dotNetDictionary;
+    }
+
+    private static List<object?> ToDotNetList(this NSArray nsArray)
+    {
+        var dotNetList = new List<object?>();
+
+        for (nuint i = 0; i < nsArray.Count; i++)
+        {
+            var nsObj = nsArray.GetItem<NSObject>(i);
+            dotNetList.Add(nsObj.ToDotNetObject());
+        }
+
+        return dotNetList;
     }
 }
