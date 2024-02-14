57bc86d3207f9ceb07542e360e513fea7e95214b
Will <willbeal@outlook.com>
2024-02-14 11:08:44 +0100

    Metadata now returns JSON string
    Updated OfferingDto to return Metadata as a JSON string as opposed to a Dictionary. Updated methods in platform implementations to convert native dictionaries back to JSON strings.

--- src/Maui.RevenueCat.InAppBilling/Platforms/Android/Extensions/OfferingsExtensions.cs
+++ src/Maui.RevenueCat.InAppBilling/Platforms/Android/Extensions/OfferingsExtensions.cs
@@ -1,6 +1,8 @@
-﻿using Android.Runtime;
+﻿using System.Collections;
 using Com.Revenuecat.Purchases;
+using Maui.RevenueCat.InAppBilling.Extensions;
 using Maui.RevenueCat.InAppBilling.Models;
+using Org.Json;
 
 namespace Maui.RevenueCat.InAppBilling.Platforms.Android.Extensions;
 
@@ -17,54 +19,14 @@ internal static class OfferingsExtensions
                 Identifier = offer.Identifier,
                 AvailablePackages = offer.AvailablePackages.ToPackageDtoList(),
                 IsCurrent = offer.Identifier == offerings?.Current?.Identifier,
-                Metadata = offer.Metadata.ToDictionary(item=>item.Key, item=>item.Value.ToDotNetObject())
+                Metadata = offer.Metadata.IsNullOrEmpty()
+                    ? null
+                    : new JSONObject((IDictionary)offer.Metadata).ToString()
             };
 
             offeringDtos.Add(offerDto);
         }
 
         return offeringDtos;
-    }
-
-    private static object? ToDotNetObject(this Java.Lang.Object? jObj) =>
-        jObj switch
-        {
-            Java.Lang.String => jObj.ToString(),
-            Java.Lang.Integer javaInteger => javaInteger.IntValue(),
-            Java.Lang.Double javaDouble => javaDouble.DoubleValue(),
-            Java.Lang.Boolean javaBoolean => javaBoolean.BooleanValue(),
-            Java.Lang.Byte javaByte=>javaByte.ByteValue(),
-            Java.Lang.Character javaChar=>javaChar.CharValue(),
-            Java.Lang.Float javaFloat=>javaFloat.FloatValue(),
-            Java.Lang.Long javaLong=>javaLong.LongValue(),
-            JavaList javaList => javaList.ToDotNetList(),
-            Java.Util.IMap javaMap => javaMap.ToDotNetDictionary(),
-            null => null,
-            _ => jObj.ToString() // fallback
-        };
-
-    private static Dictionary<object, object?> ToDotNetDictionary(this Java.Util.IMap javaMap)
-    {
-        var dotNetDictionary = new Dictionary<object, object?>();
-        var entries = javaMap.EntrySet().Cast<Java.Util.AbstractMap.SimpleImmutableEntry>();
-
-        foreach (var entry in entries)
-        {
-            dotNetDictionary.Add(entry.Key.ToDotNetObject() ?? throw new NullReferenceException("Key cannot be null."), entry.Value.ToDotNetObject());
-        }
-
-        return dotNetDictionary;
-    }
-
-    private static List<object?> ToDotNetList(this JavaList javaList)
-    {
-        var dotNetList = new List<object?>();
-
-        foreach (var item in javaList.ToArray())
-        {
-            dotNetList.Add(item.ToDotNetObject());
-        }
-
-        return dotNetList;
     }
 }
